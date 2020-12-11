1eb407d254579ef0fcc566f61495b451314040fd
Leslie Barber <[lbsnaturaltherapies@hotmail.com]>
2020-12-12 09:19:13 +1100

    created venue unihouse added the rooms for the venue

---
+++ index.rb
@@ -0,0 +1,20 @@
+#-----------------------------------------------------------------
+#
+#                       University House Room Bookings
+#                           TERMINAL APP
+#
+#                       Crafted for you by Leslie Barber
+#
+#
+# 12 December 2020
+#-----------------------------------------------------------------
+
+require 'tty-prompt'
+
+
+# create a venue with rooms to hire
+unihouse = unihouse.new.add_room(kara.new).add_room(regency.new).add_room(celadon.new).add_room(victoria.new).add_room(leather.new)
+
+# Welcome message
+
+welcome(Unihouse)
