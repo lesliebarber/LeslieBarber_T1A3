3ca6b99ed598157c78a9cc16ed68f402e04b5ece
Leslie Barber <[lbsnaturaltherapies@hotmail.com]>
2020-12-17 15:20:11 +1100

    changes to methods.rb

--- methods.rb
+++ methods.rb
@@ -1,43 +1,40 @@
 
 
-required 'tty-prompt'
+require 'tty-prompt'
 
 HEADER_LINE = "__________________________________________________"
 HEADER_LENGTH = HEADER_LINE.length
 
 
-end
+
 
 
 def back_main_menu
     puts "Press any keys to return to the main menu"
     $stdin.getch
     clear
-
-end
-
-puts HEADER_LINE
-puts "loading" .upcase.center(HEADER_LENGTH)
-puts HEADER_LINE
-puts
-
-
-puts string[0, i].center(HEADER_LENGTH)
-sleep(0.5)
-
-    end
-
-end
-
-
-def welcome (Unihouse)
-    puts
     puts HEADER_LINE
-    puts "Welcome to ".center(HEADER_LENGTH)
-    puts "{unihouse.name} Unihouse!.center(HEADER_LENGTH)"
+    puts "loading" .upcase.center(HEADER_LENGTH)
     puts HEADER_LINE
     puts
+
+
+    puts string[0, i].center(HEADER_LENGTH)
+    sleep(0.5)
+
+   
 end
+
+
+
+# def welcome(uni_house)
+#     puts
+#     puts HEADER_LINE
+#     puts "Welcome to ".center(HEADER_LENGTH)
+#     puts "#{uni_house.name} #uni_house!.center(HEADER_LENGTH)"
+#     puts HEADER_LINE
+#     puts
+# end
 
 
 def new_booking_header
@@ -61,7 +58,7 @@ def room_name_header
     clear
     puts
     puts HEADER_LINE
-    puts "#{room_name Room .upcase(HEADER_LENGTH)
+    puts "#{room_name Room .upcase(HEADER_LENGTH)}"
     puts HEADER_LINE
 
 end
